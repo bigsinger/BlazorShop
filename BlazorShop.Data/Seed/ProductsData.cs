95bdb066ff3c4354be6080310015587f316f2ecb
BIGSING <pushebp@163.com>
2022-12-12 22:22:25 +0800

    1

--- BlazorShop.Data/Seed/ProductsData.cs
+++ BlazorShop.Data/Seed/ProductsData.cs
@@ -35,7 +35,7 @@
                     Id = 3,
                     Name = "方可云仓库进销存",
                     Description = "网页版B/S在线进销存、仓库软件免安装，手机、平板、PC数据同步，只需要一个上网浏览器即可使用。集采购进货、销售、库存、财务于一体，无需培训，人人会用",
-                    ImageSource = "http://www.fkerp.com/templates/cn2014/images/1.png",
+                    ImageSource = "https://img1.baidu.com/it/u=3772627597,2855505711&fm=253&fmt=auto&app=138&f=PNG?w=500&h=250",
                     Price = 109.60m,
                     Quantity = 10,
                     CategoryId = 1
@@ -45,7 +45,7 @@
                     Id = 4,
                     Name = "苹果数据恢复精灵",
                     Description = "淘晶苹果数据恢复精灵，操作简单，无需越狱，支持所有苹果IOS设备，可恢复浏览短信、联系人、通话记录、QQ、微信、相册等数据（经测试碎片查找能力行业领先），并可轻松导出为明文备份。 本软件适配苹果官方手机助手itunes工具备份的镜像，免去进入手机内部扫描的风险。强大的镜像碎片提取引擎让您不在为删除的数据而烦恼，是一款苹果设备必备工具。",
-                    ImageSource = "http://wx.iskycn.com/image/hfios_2.jpg",
+                    ImageSource = "https://img0.baidu.com/it/u=1912789850,404984198&fm=253&fmt=auto&app=138&f=PNG?w=500&h=500",
                     Price = 366.98m,
                     Quantity = 10,
                     CategoryId = 1
@@ -55,7 +55,7 @@
                     Id = 5,
                     Name = "安卓手机数据恢复精灵",
                     Description = "支持安卓手机中的短信、联系人、通话记录、微信、QQ等常见应用的数据的导出查看及一键恢复，且只要相关碎片存在即可完全恢复出删除的数据，建议给手机ROOT权限，方便一键导出功能的使用，也可以支持部份手机免ROOT导出数据查看与恢复。",
-                    ImageSource = "http://www.taokeman.com/images/an/anp1.png",
+                    ImageSource = "https://img0.baidu.com/it/u=475388606,3830604471&fm=253&fmt=auto&app=138&f=JPEG?w=499&h=333",
                     Price = 35.99m,
                     Quantity = 10,
                     CategoryId = 1
@@ -65,7 +65,7 @@
                     Id = 6,
                     Name = "家庭电脑屏幕录制记录器",
                     Description = "一款针对电脑屏幕的录制，可用于协助您看管家中上网的孩子，了解他是否遭受到网络上不良信息的干扰，及时与孩子勾通，给他一个安全纯净的生长空间。",
-                    ImageSource = "http://wx.iskycn.com/image/small/JT01.jpg",
+                    ImageSource = "https://img2.baidu.com/it/u=2392206097,2280036916&fm=253&fmt=auto&app=138&f=GIF?w=500&h=281",
                     Price = 28.95m,
                     Quantity = 10,
                     CategoryId = 1
